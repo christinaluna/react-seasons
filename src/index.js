81893ed90c1eebdd10048dfce5f6da3b648cbdcf
christinaluna <lunachristinam@gmail.com>
2019-05-06 19:37:47 -0700

    Adds summer winter displays and styling

--- src/index.js
+++ src/index.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import SeasonDisplay from './SeasonDisplay';
+import SeasonDisplay from './SeasonDisplay';
 
 class App extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = { lat: null, errorMessage: '' };  
-    }
+    //init state object
+    state = { lat: null, errorMessage: ''};
 
     componentDidMount() {
         // get user location
@@ -17,11 +15,12 @@ class App extends React.Component {
     }
 
     render() {
+        // render content jsx
         if (this.state.errorMessage && !this.state.lat) {
             return <div>Error: {this.state.errorMessage}</div>;
         }
         if (!this.state.errorMessage && this.state.lat) {
-            return <div>Latitude: {this.state.lat}</div>;
+            return <SeasonDisplay lat={this.state.lat} />;
         }
 
         return <div>Loading!</div>;
